0e0720f3b5eef2ff720fcd7b09af45d73899f0e1
in6139fp <in6139fp@go.minnstate.edu>
2024-04-09 12:25:49 -0500

    added objects

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,11 @@
-$(function(){
-    let section1sqr = $("#section1sqr");
-    let section2rect = $("#section2rect");
-    let section3circle = $("#section3circle");
-    console.log(section1sqr);
-    console.log(section2rect);
-    console.log(section3circle);
-    // jQuery methods go here...
-gsap.to(section1sqr, {x: 200, duration: 10})
-;
-gsap.from(section2rect, {x:200, duration:10});
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.fromTo(section3circle, {x:-200, duration:10},{x:200, duration:10})});
+
+gsap.to("#square1", 
+{x: 700,
+duration: 3,
+ScrollTrigger: "#square3"})
+
 
 
 
