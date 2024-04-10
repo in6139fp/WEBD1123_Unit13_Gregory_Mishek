80e43627e1ad03917cfddef13ef09752da927927
in6139fp <in6139fp@go.minnstate.edu>
2024-04-10 14:16:24 -0500

    fixed GSAP

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,44 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
+let anton = document.querySelectorAll("#container1 > h1.anton-regular > span");
+let container1 = document.getElementById("container1");
+let container2 = document.getElementById("container2");
+let squares = document.querySelectorAll(".squares");
+console.log(squares);
 
+// gsap.to("#square1", { x: 700, duration: 3, scrollTrigger: "#square1" });
 
-gsap.to("#square1", 
-{x: 700,
-duration: 3,
-ScrollTrigger: "#square3"})
+function init() {
+  let tl1 = gsap.timeline({
+    id: "anim1",
+  });
+  tl1.fromTo("#container1", { autoAlpha: 0 }, { autoAlpha: 1, duration: 2 });
+ let tl2 = gsap.timeline({
+    id: "anim2"
+});
+  tl2.from(
+    anton,
+    {
+      x: -100,
+      y: 50,
+      autoAlpha: 0,
+      duration: 0.5,
+      stagger: 0.2,
+    },
+    "+=3"
+  );
+  let tl3 = gsap.timeline({
+    id: "anim3",
+    scrollTrigger: {
+        trigger: container2,
+        start: "top top",
+       end: "bottom center",
+       marker: true,
+       markers: {startColor: "green", endColor: "red"},
+        pin: true,
+        scrub: 1
+    }
+  })
+  tl3.from(squares, {x: 200, autoAlpha: 0, duration: 1,stagger: 0.2, ease: "power1.inOut"});
+}
 
-
-
-
- 
+window.addEventListener("load", init);
---
+++ WEBD1123_Unit13_Gregory_Mishek-main/JS/app.js
@@ -0,0 +1,12 @@
+gsap.registerPlugin(ScrollTrigger)
+
+
+gsap.to("#square1", 
+{x: 700,
+duration: 3,
+ScrollTrigger: "#square3"})
+
+
+
+
+ 
